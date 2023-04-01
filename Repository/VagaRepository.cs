d5e97d4eb0369f626754a23c2c3204a55e6a75a6
Diego Passos <diego.spassos@souunit.com.br>
2023-04-01 18:38:42 -0300

    Finalização Vagas Controller

--- Repository/VagaRepository.cs
+++ Repository/VagaRepository.cs
@@ -9,25 +9,44 @@ public class VagaRepository : IVagaRepository
     {
         _context = context;
     }
-    public Vaga Cadastrar(Vaga voluntario)
+    public Vaga Cadastrar(Vaga vaga)
     {
-        _context.Vagas.Add(voluntario);
-        return voluntario;
+        _context.Vagas.Add(vaga);
+        return vaga;
     }
 
     public void Deletar(int id)
     {
-        throw new NotImplementedException();
+        var vaga = PegarPorId(id);
+        _context.Vagas.Remove(vaga);
     }
 
-    public Vaga Editar(Vaga voluntario)
+    public Vaga Editar(Vaga vaga)
     {
-        throw new NotImplementedException();
+        var vagaEdit = PegarPorId(vaga.Id);
+
+        vagaEdit.VoluntarioId = vaga.VoluntarioId;
+        vagaEdit.Voluntario = vaga.Voluntario;
+        vagaEdit.OngId = vaga.OngId;
+        vagaEdit.Ong = vaga.Ong;
+        vagaEdit.Tipo = vaga.Tipo;
+        vagaEdit.Turno = vaga.Turno;
+        vagaEdit.Descricao = vaga.Descricao;      
+        vagaEdit.Habilidade = vaga.Habilidade;
+        vagaEdit.DataInicio = vaga.DataInicio;
+        vagaEdit.DataFim = vaga.DataFim;
+        return vagaEdit;
+
     }
 
     public Vaga PegarPorId(int id)
     {
-        throw new NotImplementedException();
+        var vaga = _context.Vagas.FirstOrDefault(x => x.Id == id);
+        if (vaga == null)
+        {
+            return new Vaga();
+        }
+        return vaga;        
     }
 
     public List<Vaga> PegarTodos()
